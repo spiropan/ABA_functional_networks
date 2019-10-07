73391d67ac958fcf3aae1b3dde2e3f00fad6dd1c
unknown <spiropan@gmail.com>
2019-10-07 00:37:13 -0400

    add distance boxplot

---
+++ helper_functions/return_XG.m
@@ -0,0 +1,16 @@
+function [X,G] = return_XG(ind,D);
+% initialize X and G, then iterate over each of 13 networks to plot
+G=[]; X=[];
+nets_ind=ind.W;
+for n=1:length(nets_ind)
+    N=ind.W_all{nets_ind(n)} % store the indeces of the nodes for each network
+    net_dis=D(N,N); % isolate the distances for nodes within each network
+    % extract lower triangle of those distances and turn into vec
+    tmp=get_indeces(length(N));
+    dis_vec=net_dis(tmp);
+
+    % Create the X and G (grouping) variable for inputs to the boxplot
+    % command
+    X=[X; dis_vec];
+    G=[G; ones(length(dis_vec),1)*nets_ind(n)];
+end
